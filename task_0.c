66728cc8ffeb98d8bc42609d2a74e6963543db19
mteeve <mteeve@gmail.com>
2023-04-26 10:01:42 -0700

    C - Simple Shell

--- task_0.c
+++ task_0.c
@@ -3,11 +3,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
 
 #define BUF_SIZE 1024
-
-static char **parse_args(char *buffer);
-static void execute_command(char **args);
 
 
 /**
@@ -20,6 +21,11 @@ int main(void)
 char *buffer = NULL;
 size_t bufsize = 0;
 ssize_t n_read = 0;
+char *args[100];
+int status = 0;
+int i = 0, j = 0;
+pid_t child_pid;
+
 
 while (1)
 {
@@ -32,39 +38,7 @@ exit(EXIT_FAILURE);
 }
 buffer[n_read - 1] = '\0';
 
-if (strcmp(buffer, "exit") == 0)
-{
-free(buffer);
-exit(EXIT_SUCCESS);
-}
 
-char **args = parse_args(buffer);
-execute_command(args);
-
-free(args);
-}
-
-return (0);
-}
-
-
-/**
-* Parses the command line arguments from the buffer.
-*
-* @param buffer The command line buffer.
-*
-* @return The arguments parsed from the buffer.
-*/
-static char **parse_args(char *buffer)
-{
-char **args = malloc(sizeof(char *) * BUF_SIZE);
-if (!args)
-{
-perror("malloc");
-exit(EXIT_FAILURE);
-}
-
-int i, j;
 for (i = 0, j = 0; buffer[i]; i++)
 {
 if (buffer[i] == ' ')
@@ -76,34 +50,35 @@ j++;
 }
 args[j] = NULL;
 
-return (args);
+
+if (strcmp(buffer, "exit") == 0)
+{
+free(buffer);
+exit(EXIT_SUCCESS);
 }
 
 
-/**
-* Executes the command with the given arguments.
-*
-* @param args The command line arguments.
-*/
-static void execute_command(char **args)
-{
-pid_t child_pid = fork();
+child_pid = fork();
 if (child_pid == -1)
 {
 perror("fork");
 exit(EXIT_FAILURE);
 }
 
+
 if (child_pid == 0)
 {
-if (execvp(args[0], args) == -1)
+if (execve(args[0], args, NULL) == -1)
 {
-perror("execvp");
+perror("./hsh");
 exit(EXIT_FAILURE);
 }
 }
 else
 {
-wait(NULL);
+wait(&status);
 }
 }
+return (0);
+}
+
